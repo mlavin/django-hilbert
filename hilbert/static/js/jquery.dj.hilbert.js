19ea2b66db87de4a0d3ff4d6407dc33f5ff9bf39
Mark Lavin <markdlavin@gmail.com>
2011-04-23 19:55:06 -0400

    Handle login required headers on the client side.

--- hilbert/static/js/jquery.dj.hilbert.js
+++ hilbert/static/js/jquery.dj.hilbert.js
@@ -1,1 +1,10 @@
 if(!window.console) console = {log: function() {}};
+
+$(function(){
+    $('body').ajaxComplete(function(event, XmlHttpRequest, ajaxOptions){
+        if (XmlHttpRequest.getResponseHeader('X-Django-Requires-Auth')) {
+            var next = window.location.pathname + window.location.hash;
+            window.location = XmlHttpRequest.getResponseHeader('X-Django-Login-Url') + '?next=' + next;
+        }
+    });
+});
