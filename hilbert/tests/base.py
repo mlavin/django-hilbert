887b4697b548598f57ba1245345786784e723b5a
mlavin <markdlavin@gmail.com>
2011-11-28 20:47:18 -0500

    Patch settings to enable SSL when running hilbert tests. Fixes #7.

--- hilbert/tests/base.py
+++ hilbert/tests/base.py
@@ -2,6 +2,7 @@
 Base test cases for Django-Hilbert.
 """
 
+from django.conf import settings
 from django.contrib.auth import models as auth
 
 from hilbert.test import TestCase
@@ -11,6 +12,15 @@ class HilbertBaseTestCase(TestCase):
     urls = 'hilbert.tests.urls'
     username = 'hilbert'
     password = 'test'
+
+    def setUp(self):
+        super(HilbertBaseTestCase, self).setUp()
+        self._ssl = getattr(settings, 'SSL_ENABLED', False)
+        settings.SSL_ENABLED = True
+        
+    def tearDown(self):
+        super(HilbertBaseTestCase, self).tearDown()
+        settings.SSL_ENABLED = self._ssl
 
     def create_user(self, data=None):
         data = data or {}
